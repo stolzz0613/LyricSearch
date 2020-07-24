732e6c84ec6b8cba0f23a74594b61a35eba9400b
stolzz0613 <andres0613@utp.edu.co>
2020-07-24 13:07:31 -0500

    Updates

---
+++ precache-manifest.d21099c8509f1ca189dcb0b990381786.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb80ce0e3f1144d4129a76fb65d3a6f",
+    "url": "/LyricSearch/index.html"
+  },
+  {
+    "revision": "c412092fa534100dd8d2",
+    "url": "/LyricSearch/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6453cae625f4941c5005",
+    "url": "/LyricSearch/static/js/2.f2bf55f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LyricSearch/static/js/2.f2bf55f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c412092fa534100dd8d2",
+    "url": "/LyricSearch/static/js/main.a7ed3f8d.chunk.js"
+  },
+  {
+    "revision": "e2ab9a7638b282843b37",
+    "url": "/LyricSearch/static/js/runtime-main.5e432f9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/LyricSearch/static/media/logo.5d5d9eef.svg"
+  }
+]);
